83aac5a2636b01807e3bf00d9deee40bb2727333
harshdeep-05 <harshdeepsahu5@gmail.com>
2021-06-18 21:27:12 +0530

    upadated expenses list

--- expenses/urls.py
+++ expenses/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='expenses'),
-    path('add_expense',views.add_expense, name='add_expense')
+    path('add_expense',views.add_expense, name='add_expense'),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Category, Expense
+from django.core.paginator import Paginator
+import datetime
 # Create your views here.
 
 def index(request):
+    categories = Category.objects.all()
+    expenses=Expense.objects.filter(owner=request.user)
+    paginator = Paginator(expenses, 5)      
+    page_number = request.GET.get('page')
+    page_obj = Paginator.get_page(paginator, page_number)
+    context={
+        'expenses':expenses,
+        'page_obj': page_obj,
+    }
     return render(request,'expenses/index.html')
 
 def add_expense(request):
-    return render(request, 'expenses/add_expense.html')
+    categories = Category.objects.all()
+    context ={
+        'categories' : categories,
+        'values': request.POST
+    }
+    return render(request, 'expenses/add_expense.html',context)
+    
+    if request.method == 'GET':
+        return render(request, 'expenses/add_expense.html', context)    
+    
+    if request.method == 'POST':
+        amount = request.POST['amount']
+    
